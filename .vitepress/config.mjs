176c52aea0888f1581dff3231a9d68d9e64b14c4
caelum714-dev <caelum714@gmail.com>
2026-04-30 19:56:15 -0400

    文件名修改

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -51,11 +51,11 @@ export default defineConfig({
             text: '基础属性篇',
             collapsed: true, // 基础属性默认折叠
             items: [
-              { text: '导言', link: '/rules/based-rule/1.character-attribute/'},
-              { text: '本格属性', link: '/rules/based-rule/1.character-attribute/standard-attributes' },
-              { text: '多维自我 (ALTER EGO, 被动能力)', link: '/rules/based-rule/1.character-attribute/alter-ego' },
-              { text: '数值属性', link: '/rules/based-rule/1.character-attribute/numerical-attributes' },
-              { text: '理念等级', link: '/rules/based-rule/1.character-attribute/character-level' },
+              { text: '导言', link: '/rules/1.based-rule/1.character-attribute/'},
+              { text: '本格属性', link: '/rules/1.based-rule/1.character-attribute/standard-attributes' },
+              { text: '多维自我 (ALTER EGO, 被动能力)', link: '/rules/1.based-rule/1.character-attribute/alter-ego' },
+              { text: '数值属性', link: '/rules/1.based-rule/1.character-attribute/numerical-attributes' },
+              { text: '理念等级', link: '/rules/1.based-rule/1.character-attribute/character-level' },
             ]
           },
 
@@ -65,12 +65,12 @@ export default defineConfig({
             collapsed: true, // 
             items: [
               { text: '导言', link: '/rules/1.based-rule/3.battle-rule/'},
-              { text: '战斗流程', link: '/rules/based-rule/3.battle-rule/1.battle process'},
-              { text: '基础回合规则', link: '/rules/based-rule/3.battle-rule/2.turn' },
-              { text: '进攻手段', link: 'rules/based-rule/3.battle-rule/3.attack' },
-              { text: '防御手段', link: '/rules/based-rule/3.battle-rule/4.defend' },
-              { text: '特殊行动', link: '/rules/based-rule/3.battle-rule/5.special' },
-              { text: '战斗距离', link: '/rules/based-rule/3.battle-rule/6.distence' },
+              { text: '战斗流程', link: '/rules/1.based-rule/3.battle-rule/1.battle process'},
+              { text: '基础回合规则', link: '/rules/1.based-rule/3.battle-rule/2.turn' },
+              { text: '进攻手段', link: 'rules/1.based-rule/3.battle-rule/3.attack' },
+              { text: '防御手段', link: '/rules/1.based-rule/3.battle-rule/4.defend' },
+              { text: '特殊行动', link: '/rules/1.based-rule/3.battle-rule/5.special' },
+              { text: '战斗距离', link: '/rules/1.based-rule/3.battle-rule/6.distence' },
             ]
           },
         ] 
@@ -85,12 +85,12 @@ export default defineConfig({
             text: '🗡️ 武器书 (按类型分类)',
             collapsed: true, // 
             items: [
-              { text: '导言', link: '/rules/equipment/weapons/'},
-              { text: '自赋能兵械', link: '/rules/equipment/weapons/weaponsZF'},
-              { text: '动能兵械', link: '/rules/equipment/weapons/weaponsDN' },
-              { text: '自导器', link: '/rules/equipment/weapons/daoqiZD' },
-              { text: '导器', link: '/rules/equipment/weapons/daoqi' },
-              { text: '祭器', link: '/rules/equipment/weapons/JiQi' },
+              { text: '导言', link: '/rules/2.equipment/weapons/'},
+              { text: '自赋能兵械', link: '/rules/2.equipment/weapons/weaponsZF'},
+              { text: '动能兵械', link: '/rules/2.equipment/weapons/weaponsDN' },
+              { text: '自导器', link: '/rules/2.equipment/weapons/daoqiZD' },
+              { text: '导器', link: '/rules/2.equipment/weapons/daoqi' },
+              { text: '祭器', link: '/rules/2.equipment/weapons/JiQi' },
             ]
           },
 
@@ -98,37 +98,37 @@ export default defineConfig({
             text: '🛡️ 防具库',
             collapsed: true,
             items: [
-              { text: '防具', link: '/rules/equipment/armors/armor'},
-              { text: '护盾', link: '/rules/equipment/armors/shield' },
-              { text: '背包', link: '/rules/equipment/armors/bag' },
+              { text: '防具', link: '/rules/2.equipment/armors/armor'},
+              { text: '护盾', link: '/rules/2.equipment/armors/shield' },
+              { text: '背包', link: '/rules/2.equipment/armors/bag' },
             ]
           }
         ]
       },
       
       {
-        text: ' 地理志',
-        collapsed: false, 
-        items: [
-          { text: '地理志总览', link: '/rules/geography/' },
-          { text: '上层区', link: '/rules/geography/upper-zone' },
-          { text: '中层区', link: '/rules/geography/middle-zone' },
-          { text: '下层区', link: '/rules/geography/lower-zone' },]
-      },
-
-      {
         text: '物品书',
         collapsed: false,
         items: [
-          { text: '载具', link: '/rules/item/vehicleDN' }
+          { text: '载具', link: '/rules/3.item/vehicleDN' }
         ]
+      },
+
+      {
+        text: ' 地理志',
+        collapsed: false, 
+        items: [
+          { text: '地理志总览', link: '/rules/4.geography/' },
+          { text: '上层区', link: '/rules/4.geography/upper-zone' },
+          { text: '中层区', link: '/rules/4.geography/middle-zone' },
+          { text: '下层区', link: '/rules/4.geography/lower-zone' },]
       },
 
       {
         text: '经济系统',
         collapsed: false,
         items: [
-          { text: '货币体系', link: '/rules/economy/currency' },
+          { text: '货币体系', link: '/rules/5.economy/currency' },
         ]
       }
 
